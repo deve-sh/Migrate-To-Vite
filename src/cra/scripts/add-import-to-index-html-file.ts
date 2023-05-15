498f0ef940447342f4ac9851020b474f7de80500
Devesh Kumar <contact@devesh.tech>
2023-05-15 12:37:37 +0530

    utils: Add function to check if file exists
    For use in adding import path to index html file

--- src/cra/scripts/add-import-to-index-html-file.ts
+++ src/cra/scripts/add-import-to-index-html-file.ts
@@ -1,13 +1,14 @@
-import fs from 'node:fs';
-import path from 'node:path';
+import fs from "node:fs";
+import path from "node:path";
 
 interface Args {
-    root: string;
+	root: string;
 }
 
 const addImportToIndexHTMLFile = ({ root }: Args) => {
-    const htmlContent = fs.readFileSync(path.resolve(process.cwd(), './index.html'), 'utf-8');
-    // TODO: Add a script tag referencing the index.(ts/js/tsx/jsx) file in the root directory.
+	const htmlContent = fs.readFileSync(path.resolve(process.cwd(), "./index.html"), "utf-8");
+	// TODO: Add a script tag referencing the index.(ts/js/tsx/jsx) file in the root directory.
+	const tag = `<script type=""></script>`;
 };
 
 export default addImportToIndexHTMLFile;
