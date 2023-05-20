85b11067ee7ac9e769c1e4cbb5d88d9255773268
Devesh Kumar <devesh2027@gmail.com>
2023-05-20 22:18:38 +0530

    fix: CRA: TS Config update

--- src/cra/scripts/update-tsconfig.ts
+++ src/cra/scripts/update-tsconfig.ts
@@ -25,9 +25,9 @@ const updateTSConfigFile = () => {
 			"types",
 			"vite-plugin-svgr/client"
 		);
-		addToArrayIfNotExists(tsConfig.compilerOptions, "libs", "dom");
-		addToArrayIfNotExists(tsConfig.compilerOptions, "libs", "dom.iterable");
-		addToArrayIfNotExists(tsConfig.compilerOptions, "libs", "esnext");
+		addToArrayIfNotExists(tsConfig.compilerOptions, "lib", "dom");
+		addToArrayIfNotExists(tsConfig.compilerOptions, "lib", "dom.iterable");
+		addToArrayIfNotExists(tsConfig.compilerOptions, "lib", "esnext");
 		fs.writeFileSync("./tsconfig.json", JSON.stringify(tsConfig, null, 2));
 	} catch {
 		// Might not have a tsconfig.json file
