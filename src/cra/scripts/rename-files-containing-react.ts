d1d969f63fb09c0e6aaee66cb1b66e847fd8f945
Devesh Kumar <devesh2027@gmail.com>
2023-05-20 11:04:40 +0530

    fix: Renaming of React files

--- src/cra/scripts/rename-files-containing-react.ts
+++ src/cra/scripts/rename-files-containing-react.ts
@@ -8,10 +8,12 @@ interface Args {
 
 const renameFilesContainingReact = async ({ root }: Args) => {
 	console.log(chalk.blue("Renaming files containing JSX"));
-	const files = await recursive(root, [
-		(file) => !file.endsWith(".ts") && !file.endsWith(".js"),
-	]);
+	const files = await recursive(root);
 	files.forEach((file) => {
+		const isNotJSFile = !file.endsWith(".ts") && !file.endsWith(".js");
+		const isDirectory = fs.statSync(file).isDirectory();
+		if (isDirectory || isNotJSFile) return;
+		
 		const content = fs.readFileSync(file, "utf-8");
 		if (content.includes("import React")) return fs.renameSync(file, file + "x");
 		if (content.includes("</")) return fs.renameSync(file, file + "x");
